40b242857754b0f9d89f19d0d8a304a5ed03075b
IngaGlotzbach <ingaglotzbach@googlemail.com>
2022-01-04 01:05:19 +0100

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,22 @@ import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { RouterModule } from "@angular/router";
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeadlineComponent } from './headline/headline.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CvComponent } from './cv/cv.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LandingPageComponent, NavBarComponent],
+  declarations: [AppComponent, HomeComponent, LandingPageComponent, NavBarComponent, HeadlineComponent, AboutComponent, ProjectsComponent, CvComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       { path: "", component: HomeComponent },
       { path: "home", component: LandingPageComponent },
+      { path: "about", component: AboutComponent },
+      { path: "projects", component: ProjectsComponent },
+      { path: "cv", component: CvComponent }
     ]),
     NgbModule,
   ],
